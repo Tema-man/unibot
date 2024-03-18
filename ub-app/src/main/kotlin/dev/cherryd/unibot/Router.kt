998d3a36c90494ec68260712a1d5c48e369a57a7
Artemii Vishnevskii <temaa.mann@gmail.com>
2024-03-18 13:32:39 +0100

    Update routing

--- ub-app/src/main/kotlin/dev/cherryd/unibot/Router.kt
+++ ub-app/src/main/kotlin/dev/cherryd/unibot/Router.kt
@@ -2,19 +2,17 @@ package dev.cherryd.unibot
 
 import dev.cherryd.unibot.core.Posting
 import dev.cherryd.unibot.core.Responder
-import dev.cherryd.unibot.responder.security.AntiDdosProtector
 
-class Router(
-    private val responders: List<Responder>,
-    private val antiDdosProtector: AntiDdosProtector
-) {
+class Router(private val responders: List<Responder>) {
+
     fun pickResponder(posting: Posting): Responder {
-        val availableTransformers = responders
-            .filter { (it.getPriority(posting.settings) != Responder.Priority.DISABLED) && it.canHandle(posting) }
-            .ifEmpty { listOf(antiDdosProtector) }
+        val enabledTransformers = responders
+            .filter { (it.getPriority(posting.settings) != Responder.Priority.DISABLED) }
+
+        val transformers = enabledTransformers.filter { it.canHandle(posting) }
             .sortedByDescending { it.getPriority(posting.settings) }
 
-        val processor = availableTransformers.firstOrNull() ?: availableTransformers.random()
+        val processor = transformers.firstOrNull() ?: enabledTransformers.random()
         return processor
     }
 }
