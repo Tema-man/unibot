bacc186b6dec41ff07d80c218295c88054a14e7d
Artemii Vishnevskii <temaa.mann@gmail.com>
2024-06-21 12:35:18 +0200

    Fix reply parsing

--- bot-telegram/src/main/kotlin/dev/cherryd/unibot/telegram/PostingMediaMapper.kt
+++ bot-telegram/src/main/kotlin/dev/cherryd/unibot/telegram/PostingMediaMapper.kt
@@ -1,7 +1,9 @@
 package dev.cherryd.unibot.telegram
 
+import dev.cherryd.unibot.core.Chat
 import dev.cherryd.unibot.core.Posting
 import dev.cherryd.unibot.core.Settings
+import dev.cherryd.unibot.core.User
 import dev.cherryd.unibot.telegram.parser.ExtraParser
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
@@ -9,26 +11,44 @@ import org.telegram.telegrambots.meta.api.objects.Update
 class PostingMediaMapper(
     private val parsers: List<ExtraParser>
 ) {
-    fun map(update: Update, settings: Settings): Posting.Content {
-        return mapMessage(update, settings, update.message)
-    }
+    fun map(update: Update, settings: Settings): Posting.Content = mapMessage(
+        message = update.message,
+        sender = update.toUser(settings),
+        chat = update.getUniBotChat(),
+        extra = parseExtra(update, settings),
+        settings = settings
+    )
 
-    private fun mapMessage(update: Update, settings: Settings, message: Message?): Posting.Content {
-        return Posting.Content(
-            id = message?.messageId.toString(),
-            sender = update.toUser(settings),
-            chat = update.getUniBotChat(),
-            extra = parseExtra(update, settings),
-            reply = parseReply(update, settings)
-        )
-    }
+    private fun mapMessage(
+        message: Message?,
+        sender: User,
+        chat: Chat,
+        extra: Posting.Content.Extra,
+        settings: Settings,
+    ): Posting.Content = Posting.Content(
+        id = message?.messageId.toString(),
+        sender = sender,
+        chat = chat,
+        extra = extra,
+        reply = parseReply(message, chat, settings)
+    )
 
     private fun parseExtra(update: Update, settings: Settings) =
         parsers.firstNotNullOfOrNull { it.parse(update, settings) }
             ?: Posting.Content.Extra.Text(update.text)
 
-    private fun parseReply(update: Update, settings: Settings): Posting.Content? {
-        val replyToMessage = update.message.replyToMessage ?: return null
-        return mapMessage(update, settings, replyToMessage)
+    private fun parseReply(
+        message: Message?,
+        chat: Chat,
+        settings: Settings,
+    ): Posting.Content? {
+        val replyMessage = message?.replyToMessage ?: return null
+        return mapMessage(
+            message = replyMessage,
+            sender = replyMessage.from.toUser(settings),
+            chat = chat,
+            extra = Posting.Content.Extra.Text(message.text),
+            settings = settings
+        )
     }
 }
--- bot-telegram/src/main/kotlin/dev/cherryd/unibot/telegram/UpdateExt.kt
+++ bot-telegram/src/main/kotlin/dev/cherryd/unibot/telegram/UpdateExt.kt
@@ -6,6 +6,7 @@ import dev.cherryd.unibot.core.User
 import org.telegram.telegrambots.meta.api.objects.InaccessibleMessage
 import org.telegram.telegrambots.meta.api.objects.Message
 import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.api.objects.User as TgUser
 
 internal fun Update.getUniBotChat(): Chat {
     val tgChat = when {
@@ -37,17 +38,23 @@ internal fun Update.toUser(settings: Settings): User {
         hasPreCheckoutQuery() -> preCheckoutQuery.from
         else -> throw IllegalArgumentException("Cant process $this")
     }
+    val isAdmin = kotlin.runCatching {
+        chatMember.newChatMember.status in setOf("administrator", "creator")
+    }.getOrDefault(false)
 
-    val formattedName = (user.firstName.let { "$it " }) + (user.lastName ?: "")
+    return user.toUser(settings, isAdmin)
+}
+
+internal fun TgUser.toUser(settings: Settings, isAdmin: Boolean = false): User {
+    val formattedName = listOfNotNull(firstName, lastName).joinToString(" ").trim()
     val role = when {
-        settings.developerName == user.userName -> User.Role.DEVELOPER
-        user.isBot -> User.Role.BOT
-        chatMember.newChatMember.status in setOf("administrator", "creator") -> User.Role.ADMIN
+        settings.developerName == userName -> User.Role.DEVELOPER
+        isBot -> User.Role.BOT
+        isAdmin -> User.Role.ADMIN
         else -> User.Role.USER
     }
-
     return User(
-        id = user.id.toString(),
+        id = id.toString(),
         role = role,
         name = formattedName,
     )
